b741b645f6e23a69dca46a5783b17678dbdb0d2e
lacramioara <runcan_lacramioara@yahoo.com>
2020-10-26 09:41:13 +0200

    Adaugare remove function
    Signed-off-by: lacramioara <runcan_lacramioara@yahoo.com>

--- src/ro/mta/facc/slab/Biblioteca.java
+++ src/ro/mta/facc/slab/Biblioteca.java
@@ -3,18 +3,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Biblioteca {
-    private ArrayList<Carte> carti;
+    private ArrayList<Carte> listaCarti;
 
     public Biblioteca() {
-        carti=new ArrayList<Carte>();
+        this.listaCarti =new ArrayList<Carte>();
     }
 
-    public Biblioteca(ArrayList<Carte> carti) {
-        carti=new ArrayList<Carte>();
+    public Biblioteca(ArrayList<Carte> listaCarti) {
+        this.listaCarti =new ArrayList<Carte>();
 
-       for(Carte c :carti)
+       for(Carte c : listaCarti)
        {
-           this.carti.add(c);
+           this.listaCarti.add(c);
        }
     }
 
@@ -23,7 +23,7 @@ public class Biblioteca {
 
         StringBuilder builder=new StringBuilder();
         builder.append("Biblioteca contine:\n");
-        for(Carte c :carti)
+        for(Carte c : listaCarti)
         {
             builder.append(c.toString());
             builder.append("\n");
@@ -34,23 +34,28 @@ public class Biblioteca {
 
     public void add(Carte c)
     {
-        this.carti.add(c);
+        this.listaCarti.add(c);
     }
     public void sort()
     {
-        Collections.sort(carti);
+        Collections.sort(listaCarti);
+    }
+
+    public void remove_book(Carte c)
+    {
+        listaCarti.remove(c);
     }
 
     public int nr_carti() {
         int nr=0;
-        nr=this.carti.size();
+        nr=this.listaCarti.size();
         return nr;
     }
 
 
     public ArrayList<Carte> get_carti()
     {
-        return this.carti;
+        return this.listaCarti;
     }
 
 
